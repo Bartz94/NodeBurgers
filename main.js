344b684d1e52e4789de828a34365cd2a157f6841
Bartosz Czyzewski <bart94946@gmail.com>
2023-12-30 15:34:10 +0100

    connect with mongo db, add get and create burgor endpoints

--- main.js
+++ main.js
@@ -1,35 +1,70 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Burger = require('./models/burger')
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongo db
+const dbURI = 'mongodb+srv://user:test12345@nodeburgers.fqid5mt.mongodb.net/nodeburgers?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => { console.log(err) })
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
+// routes
+app.get('/all-burgers', (req, res) => {
+    Burger.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.post('/burgers', (req, res) => {
+    const newBurger = new Burger(req.body)
+    newBurger.save()
+        .then((result) => {
+            res.redirect('/burgers')
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+
+// basic routes
 app.get('/', (req, res) => {
-    const burgers = [
-        { name: 'Classic Burger', ingredients: ['beef', 'bbq sauce', 'onion', 'cheddar', 'lettuce', 'tomato'], price: 24 },
-        { name: 'Chicken Burger', ingredients: ['chicken', 'bbq sauce', 'onion', 'cheddar', 'lettuce', 'cucumber'], price: 22 },
-        { name: 'Spicy burger', ingredients: ['beef', 'spicy mayo sauce', 'onion', 'cheddar', 'lettuce', 'tomato'], price: 25 },
-    ];
-    res.render('main', { title: 'Home', burgers });
+    res.redirect('/burgers')
 });
 
 app.get('/about', (req, res) => {
     res.render('aboutUs', { title: 'About' });
 });
 
-app.get('/burgers/create', (req, res) => {
+// burger routes
+app.get('/burgers', (req, res) => {
+    Burger.find()
+        .then((result) => {
+            res.render('main', { title: 'Home', burgers: result });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+app.get('/create', (req, res) => {
     res.render('createBurger', { title: 'New burger' });
 });
 
-// 404 page
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
